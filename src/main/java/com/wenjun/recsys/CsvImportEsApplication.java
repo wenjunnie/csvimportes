beaaa1db5ff6ae1c16526ab80caf1dcef89413e7
wenjunnie <1287018840@qq.com>
2020-01-07 23:42:23 +0800

    初始化项目

---
+++ src/main/java/com/wenjun/recsys/CsvImportEsApplication.java
@@ -0,0 +1,13 @@
+package com.wenjun.recsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvImportEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvImportEsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wenjun/recsys/CsvImportEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenjun.recsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvImportEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
